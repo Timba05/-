d65650eaef75be16042f167e5c21e5c8785483f6
Timba05 <sokoltim208@gmail.com>
2024-12-03 15:52:01 +0300

    первый коммит

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace var8zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string sentence = Console.ReadLine();
+
+            string processedSentence = RemoveShortWords(sentence);
+
+            Console.WriteLine("\nОбработанное предложение:");
+            Console.WriteLine(processedSentence);
+            Console.ReadKey();
+        }
+
+        static string RemoveShortWords(string sentence)
+        {
+            // Разбиваем предложение на слова, используя пробелы как разделители
+            string[] words = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Фильтруем слова, оставляя только те, которые имеют длину 3 и более символов
+            string[] filteredWords = words.Where(word => word.Length >= 3).ToArray();
+
+
+            // Объединяем отфильтрованные слова обратно в предложение
+            return string.Join(" ", filteredWords);
+        }
+    }
+}
+    
